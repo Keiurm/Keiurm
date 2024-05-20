7ca50cb0242d65e955e70805349bc5a9645ce760
Keisuke Kimura <kimurakeisukej@gmail.com>
2024-05-20 13:37:15 +0900

    fix: フォーマッターで書体を修正

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-
 var bar = new ProgressBar.Line(splash_text, {
-  easing: 'easeInOut',
+  easing: "easeInOut",
   duration: 1000,
   strokeWidth: 0.2,
-  color: '#555',
+  color: "#555",
   trailWidth: 0.2,
-  trailColor: '#bbb',
-  text: {      
+  trailColor: "#bbb",
+  text: {
     style: {
-      position: 'absolute',
-      left: '50%',
-      top: '50%',
-      padding: '0',
-      margin: '-30px 0 0 0',
-      transform:'translate(-50%,-50%)',
-      'font-size':'1rem',
-      color: '#fff',
+      position: "absolute",
+      left: "50%",
+      top: "50%",
+      padding: "0",
+      margin: "-30px 0 0 0",
+      transform: "translate(-50%,-50%)",
+      "font-size": "1rem",
+      color: "#fff",
     },
-    autoStyleContainer: false 
+    autoStyleContainer: false,
   },
-  step: function(state, bar) {
-    bar.setText(Math.round(bar.value() * 100) + ' %'); 
-  }
+  step: function (state, bar) {
+    bar.setText(Math.round(bar.value() * 100) + " %");
+  },
 });
 
 //アニメーションスタート
 bar.animate(1.0, function () {
   $("#splash").delay(500).fadeOut(800);
-});  
+});
